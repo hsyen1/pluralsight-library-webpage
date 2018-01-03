57ed28327df6429e08edf3f62c64e458ef0c4553
hsyen1 <hsyen1@student.monash.edu>
2018-01-03 15:13:47 +0800

    N/A

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var app = express();
+var sql = require('mssql');
+var config = {
+    user: 'sa',
+    password: '1234',
+    server: 'MYKLNB-52\\SQLEXPRESS',
+    database: 'tempdb'
+};
+
+sql.connect(config, function(err) {
+    console.log(err);
+});
+
 var port = process.env.PORT || 5000;
-var bookRouter = require('./src/routes/bookRoutes');
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'}, {
+    Link: '/Authors',
+    Text: 'Author'}];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-
-
 app.use('/Books', bookRouter);
 
 //Express taking request from the browser
@@ -22,14 +37,6 @@ app.get('/', function(req, res) {
             Text: 'Authors'
         }]
     });
-});
-
-app.get('/books', function(req, res) {
-    res.send('Hello Books');
-});
-
-app.get('/authors', function(req, res) {
-    res.send('Hello Authors');
 });
 
 app.listen(port, function(err) {
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,69 +1,72 @@
 var express = require('express');
-
 var bookRouter = express.Router();
+var sql = require('mssql');
 
-var books = [
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    },
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    },
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    },
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    },
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    }
-];
+var router = function(nav) {
+    var books = [
+        {
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        }
+    ];
+    //this routes to the nav bar
+    //Link: sent to the ejs file to make it click-able by adding it to the href
+    //Text: the text of the nav bar
+    //books are sent into bookListView's stories
+    bookRouter.route('/')
+        .get(function(req, res) {
+            var request = new sql.Request();
+            request.query('select * from books',
+                function(err, recordset) {
+                    //deletes the extra properties as HTML can only read in a list to render
+                    delete recordset.recordsets;
+                    delete recordset.output;
+                    delete recordset.rowsAffected;
+                    res.render('bookListView', {
+                        title: 'Books',
+                        nav: nav,
+                        books: recordset['recordset']
+                    });
+                });
 
-bookRouter.route('/')
-    .get(function(req, res) {
-        res.render('bookListView', {
-            title: 'Books',
-            nav: [{
-                Link: '/Books',
-                Text: 'Books'
-            }, {
-                Link: '/Authors',
-                Text: 'Authors'
-            }],
-            books: books
         });
-    });
-
-bookRouter.route('/:id')
-    .get(function(req, res) {
-        var id = req.params.id;
-        res.render('bookView', {
-            title: 'Books',
-            nav: [{
-                Link: '/Books',
-                Text: 'Books'
-            }, {
-                Link: '/Authors',
-                Text: 'Authors'
-            }],
-            book: books[id]
+    //routes to each of the book page according to their id
+    bookRouter.route('/:id')
+        .get(function(req, res) {
+            var id = req.params.id;
+            res.render('bookView', {
+                title: 'Books',
+                nav: nav,
+                book: books[id]
+            });
         });
-    });
+    return bookRouter;
+};
 
-module.exports = bookRouter;
+module.exports = router;
