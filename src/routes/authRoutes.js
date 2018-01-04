8764adae22389ef8f22c99483b2884661d3eb71d
hsyen1 <hsyen1@student.monash.edu>
2018-01-04 17:32:59 +0800

    new routing js file for authentication/signing up

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var authRouter = express.Router();
+
+var router = function() {
+    authRouter.route('/signUp')
+        .post(function(req, res) {
+            console.log(req.body);
+        });
+    return authRouter;
+};
+
+module.exports = router;
