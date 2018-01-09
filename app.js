701d805d3da95501c4f823595f93a8235a79b11c
hsyen1 <hsyen1@student.monash.edu>
2018-01-09 16:50:07 +0800

    - added cookie parser

--- app.js
+++ app.js
@@ -26,8 +26,12 @@ var bookRouter = require('./src/routes/bookRoutes')(nav);
 var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //parses JSON from the body into objects
 app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+
+require('./src/config/passport')(app);
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
@@ -52,3 +56,4 @@ app.get('/', function(req, res) {
 app.listen(port, function(err) {
     console.log('Running server on port ' + port);
 });
+
